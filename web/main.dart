da78f1dc4438f79a3fe82ef9d524957bd39ea0f7
Rohan Likhite <rlikhite@gmail.com>
2016-01-09 17:08:55 -0500

    core working

--- web/main.dart
+++ web/main.dart
@@ -6,16 +6,20 @@ ButtonElement generateButton;
 Element inputText;
 Element albumArt;
 Element inputView;
+Element albumStyle;
+Element setAlbumTitleStyle;
+Element setAlbumArtistStyle;
 
 void main() {
-	generateButton = querySelector("#generate-album-btn");
+	generateButton = querySelector('#generate-album-btn');
   generateButton.onClick.listen(updateAlbum);
+  querySelector('#album-style').onInput.listen(changeStyle);
 }
 
 void updateAlbum(Event e) {
-	inputText = querySelector("#artist-input");
-	inputView = querySelector(".input-view");
-	albumArt = querySelector(".render-view");
+	inputText = querySelector('#artist-input');
+	inputView = querySelector('.input-view');
+	albumArt = querySelector('.render-view');
   String inputName = inputText.value;
   setArtistName(inputName);
   generateAlbumName();
@@ -34,3 +38,19 @@ void generateAlbumName() {
 	_albumName = albumNames[randomIndex.nextInt(albumNames.length)];
 	querySelector('.album-name').text = _albumName;	
 }
+
+void changeStyle(Event e){
+	String albumStyle = 'style-' + querySelector('#album-style').value;
+	setAlbumTitleStyle = querySelector('.album-name');
+	setAlbumArtistStyle = querySelector('.artist-name');
+	String currentStyle = setAlbumTitleStyle.dataset['style'];
+
+
+	setAlbumTitleStyle.classes.remove(currentStyle);
+	setAlbumArtistStyle.classes.remove(currentStyle);
+	setAlbumTitleStyle.classes.add(albumStyle);
+	setAlbumArtistStyle.classes.add(albumStyle);
+	setAlbumTitleStyle.dataset['style'] = albumStyle;
+	setAlbumArtistStyle.dataset['style'] = albumStyle;
+}
+
