d80aa511126c4bb5d291a0af6d5d6c6d5e5b7218
Rohan Likhite <rlikhite@gmail.com>
2016-01-09 13:32:20 -0500

    got random album

--- web/main.dart
+++ web/main.dart
@@ -1,28 +1,26 @@
 import 'dart:html';
+import 'dart:math' show Random;
 
 ButtonElement genButton;
 
 void main() {
   querySelector('#artist-input').onInput.listen(updateAlbum);
-  genButton = querySelector('#generateButton');
-  genButton.onClick.listen(generateBadge);
 }
 
 void updateAlbum(Event e) {
   String inputName = (e.target as InputElement).value;
-  setAlbumName(inputName);
-  // setBadgeName(inputName);
-  // if (inputName.trim().isEmpty) {
-  //   genButton
-  //     ..disabled = false
-  //     ..text = 'Enter';
-  // } else {
-  //   genButton
-  //     ..disabled = true
-  //     ..text = 'Ok';
-  // }
+  setArtistName(inputName);
+  generateAlbumName();
 }
 
-void setAlbumName(String newName) {
+void setArtistName(String newName) {
   querySelector('.artist-name').text = newName;
+}
+
+generateAlbumName() {
+	var randomIndex = new Random();
+	List albumNames =  ['one', 'two'];
+	String _albumName;
+	_albumName = albumNames[randomIndex.nextInt(albumNames.length)];
+	querySelector('.album-name').text = _albumName;	
 }
